6bb2165a4bb2b7fcae391381d7f094c24ce7f6aa
CandiceBrasileiro <candicelopes88@gmail.com>
2023-05-03 09:36:07 -0300

    estilização com tailwind

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,11 +5,11 @@ import '../index.css';
 const Nav = () => {
 
   return (
-    <div className='box-border bg-[#fff] max-w-7xl mx-auto'>
+    <div className='box-border bg-[#fff] max-w-7xl mx-auto '>
   <nav className='flex justify-between #94a3b8 items-center shadow-md' >
-    <img src={logo} alt="Logomarca da página publishIn" />
+    <img src={logo} alt="Logomarca da página publishIn" className='ml-3'/>
     <input 
-    className='rounded-lg  w-40 h-9 bg-[#ff735c] border-slate-300 hover:border-slate-400 text-white shadow-md' 
+    className='rounded-lg  w-40 h-9 bg-[#ff735c] cursor-pointer border-slate-300 hover:border-slate-400 text-white shadow-md mr-3' 
     name='btn'
     type='submit'
     value='Login'
--- src/components/Register.js
+++ src/components/Register.js
@@ -35,20 +35,21 @@ const Register = () => {
 
   return ( 
   <>
-    <main>
-    <section className='box-border bg-[#fff] max-w-7xl mx-auto flex mt-10' >
+    <main className=' bg-[#fff] max-w-7xl mx-auto flex mt-10 gap-5 items-center '>
+    <section  >
       <img src={Imagem} alt="Desenho colorido de livros, tinta, mulher, caneta tinteiro" className='max-w-[100%]' />
     </section>
-    <section>
-      <img src={logo} alt="Logomarca da página publishIn" />
-      <form action="" onSubmit={cadastrarUsuario}>
-        <div>
-          <label htmlFor="email">E-mail</label>
+    <section className=' w-80 h-96 shadow-lg shadow-gray-500/50 rounded-md stroke-[#A8A5A5] '>
+      <img src={logo} alt="Logomarca da página publishIn" className='mx-auto' />
+      <form action="" onSubmit={cadastrarUsuario} className='ml-10 mt-10'>
+        <div >
+          <label htmlFor="email" className='mt-1.5 mb-2 text-sm font-medium text-gray-900'>E-mail</label>
           <br />
           <input 
+          className="block w-60 h-8 mt-1.5 mb-2 text-sm text-[#385A64] font-thin border border-gray-300 rounded-md cursor-text bg-[#F3F3F3] dark:text-gray-400 focus:outline-none shadow-lg shadow-gray-500/50 pl-2 "
           type="email"
           name='email'
-          placeholder='E-mail'
+          placeholder='Digite seu e-mail'
           value={email || ""}
           onBlur={handleBlur}
           onChange={(e) => setEmail(e.target.value)}
@@ -56,32 +57,33 @@ const Register = () => {
           />
         </div>
         <div>
-          <label htmlFor="password">Senha</label>
-          <br />
-          <input type="password"
+          <label htmlFor="password" className='text-sm font-medium text-gray-900'>Senha</label>
+          
+          <input 
+          className="block w-60 h-8 mt-1.5 mb-2 text-sm text-[#385A64] font-thin border border-gray-300 rounded-md cursor-text bg-[#F3F3F3] dark:text-gray-400 focus:outline-none shadow-lg shadow-gray-500/50 pl-2 "
+          type="password"
            name='password'
-           placeholder='Senha'
+           placeholder='Digite sua senha'
            value={password || ""}
            onChange={(e) => setPassword(e.target.value)}
            required
           />
         </div>
         <div>
-          <label htmlFor="confirm">Confirme senha</label>
-          <br />
-          <input type="password"
+          <label htmlFor="confirm" className='mt-1.5 mb-2 text-sm font-medium text-gray-900'>Confirme senha</label>
+          <input 
+          className="block w-60 h-8 mt-1.5 mb-2 text-sm text-[#385A64] font-thin border border-gray-300 rounded-md cursor-text bg-[#F3F3F3] dark:text-gray-400 focus:outline-none shadow-lg shadow-gray-500/50 pl-2 "
+          type="password"
            name='Confirm'
-           placeholder='Confirme Senha'
-           
+           placeholder='Confirme sua senha'
            value={confirm || ""}
            onChange={(e) => setConfirm(e.target.value)}
            required
           />
         </div>
-        <br />
-        <input type="submit" value="Cadastre-se" />
+        
+        <input type="submit" value="Cadastre-se" className='mt-4 rounded-lg w-40 h-9 bg-[#385A64]  cursor-pointer border-slate-300 hover:border-slate-400 items-center text-white shadow-lg shadow-gray-500/50 ml-9'/>
       </form>
-      
     </section>
     </main>
     </>
